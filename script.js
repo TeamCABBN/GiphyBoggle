2a464422787007218e24c5b357f7e5bc917cd77d
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-03-30 20:06:07 +1030

    Timer done

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const queryBoggleAPI = (letters) => {
         },
         //Function to run on successful response
         success: (response) => {
-            
+
             //Testing
             console.log(response);
             output.innerText = "";
@@ -32,7 +32,7 @@ const queryBoggleAPI = (letters) => {
                 answerWords.push(word.toLowerCase());
 
                 //Testing
-                output.innerText += word.toLowerCase()+ "\n";
+                output.innerText += word.toLowerCase() + "\n";
             });
 
             //Testing
@@ -46,10 +46,92 @@ const queryBoggleAPI = (letters) => {
     });
 }
 
+
+//Timer object
+/*
+To start timer call, 
+timer.start()
+
+To stop timer call, 
+timer.stop()
+
+to reset timer call,
+timer.reset()
+
+*/
+const timer = {
+    //Default start seconds
+    startSecs: 180,
+    //Tracks the current
+    currSecs: 180,
+    increment: 1000,
+    timerRunning: false,
+
+    //Start timer Fn
+    start: function () {
+        //set timer running to true
+        this.timerRunning = true;
+
+        //interval function to run until finish
+        var intervalFn = setInterval(() => {
+
+            //if the timer is running continue
+            if (this.timerRunning) {
+
+                //decrement
+                this.currSecs -= 1;
+
+                //Display on page
+                this.display();
+
+                //if reached 0 stop timer and call endgame fn
+                if (this.currSecs <= 0) {
+                    this.timerRunning = false;
+
+                    //call end game events after small delay otherwise display isn't updated
+                    setTimeout(endGame, 4);
+                }
+            } else {
+                //if timer is not running clears interval
+                clearInterval(intervalFn);
+            }
+        }, this.increment);
+    },
+
+    //stop timer function
+    stop: function () {
+        this.timerRunning = false;
+    },
+
+    //reset timer function
+    reset: function (seconds) {
+        this.startSecs = seconds;
+        this.currSecs = this.startSecs;
+        this.display();
+    },
+
+    //Fn to display secs to page
+    display: function () {
+        console.log(this.currSecs);
+    }
+}
+
 //For testing purposes
 queryBtn.addEventListener("click", (event) => {
 
     let queryLetters = inputEl.value;
     queryBoggleAPI(queryLetters);
 
-})
+});
+
+document.querySelector(".startTimer").addEventListener("click", () => {
+    timer.start();
+});
+
+document.querySelector(".stopTimer").addEventListener("click", () => {
+    timer.stop();
+});
+
+document.querySelector(".resetTimer").addEventListener("click", () => {
+    timer.reset();
+});
